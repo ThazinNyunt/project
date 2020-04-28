f230e86201717dba674dd1f89e98afa9b032e8f3
ThazinNyunt <thazinnyunt06@gmail.com>
2020-04-28 13:40:20 +0630

    update questions.php

--- questions.php
+++ questions.php
@@ -7,10 +7,22 @@ $courseId =$_GET['course_id'];
 $rows = getQuestion($courseId);
 //print_r($rows);
 
+if(isset($_POST['btnsubmit']))
+{
+    $question1 = $_POST['question-1'];
+    $question2 = $_POST['question-2'];
+
+    echo $question1;
+    echo $question2;
+    //header("Location: checkanswer.php"); 
+}
+
 ?>
 <div class="container bg-white">
 
 <h1 align="center">Questions</h1>
+<p style="float: right;">Total Question: <?php echo count($rows); ?></p>
+<br/>
 <?php foreach($rows as $row): ?>
     <div >
         Q: <b><?php echo $row['question_text'];?></b><br>
@@ -24,14 +36,16 @@ $rows = getQuestion($courseId);
         ?>
         <?php for($i=1; $i<5; $i++): ?>
                 <div>
-                    <input type="radio" class="form-control"
-                        name="question-<?php echo $row['question_id'];?>" value="<?php echo $i; ?>">
-                    <?php echo $answers[$i]; ?>
+                    <label class="radio-inline">
+                        <input type="radio" name="question-<?php echo $row['question_id'];?>" value="<?php echo $i; ?>">
+                            <?php echo $answers[$i]; ?>
+                    </label>
+                    
                 </div>      
         <?php endfor;?>           
     </div>
     <br>
 <?php endforeach; ?>
-Total Question: <?php echo count($rows); ?>
+<button type="submit" name="btnsubmit" class="btn btn-primary">Check Answer</button>
 </div>
 
